ce54d45491b041677f5e00e71698f1e0bd5da9f4
jujuko75 <yfukuju75@gmail.com>
2021-01-16 21:13:01 +0900

    不要ファイルの削除及び単体コード追加
    Specのfactories及びmodelsの内、DBテーブル作成していないモデルのファイルを削除。

--- spec/factories/postagetypes.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :postagetype do
-    
-  end
-end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -43,25 +43,50 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
       end
+      it "category_idが1の場合登録できない" do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
       it "condition_idがないと登録できない" do
         @item.condition_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank", "Condition is not a number")
+      end
+      it "condition_idが1の場合登録できない" do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
       it "postagetype_idがないと登録できない" do
         @item.postagetype_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Postagetype can't be blank", "Postagetype is not a number")
       end
+      it "postagetype_idが1の場合登録できない" do
+        @item.postagetype_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postagetype must be other than 1")
+      end
       it "prefecture_idがないと登録できない" do
         @item.prefecture_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
       end
+      it "prefecture_idが1の場合登録できない" do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
       it "deliveryday_idがないと登録できない" do
         @item.deliveryday_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Deliveryday can't be blank", "Deliveryday is not a number")
+      end
+      it "deliveryday_idが1の場合登録できない" do
+        @item.deliveryday_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Deliveryday must be other than 1")
       end
       it "priceがないと登録できない" do
         @item.price = ""
@@ -69,12 +94,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it "priceが299以下だと登録できない" do
-        @item.price = "299"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than 299")
       end
       it "priceが10000000以上だと登録できない" do
-        @item.price = "100000000"
+        @item.price = 100000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than 10000000")
       end
@@ -87,6 +112,16 @@ RSpec.describe Item, type: :model do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("User must exist")
+      end
+      it "priceが半角英数混合の場合登録できない" do
+        @item.price = "300aa0"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "priceが半角英語の場合登録できない" do
+        @item.price = "aaaaaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
     end
   
